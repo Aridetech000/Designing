3dfa8a45b2e08455a27561de66fa669561ab1750
Aridetech000 <140967399+Aridetech000@users.noreply.github.com>
2023-07-30 21:27:03 +0530

    Products Designing

---
+++ smoothscrolling.js
@@ -0,0 +1,101 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+function firstPageAnim() {
+    var t1 = gsap.timeline();
+
+    t1.from("#nav", {
+        y: '-10',
+        opacity: 0,
+        duration: 1.5,
+        ease: Expo.easeInOut
+    })
+
+    t1.to(".boundingelem", {
+        y: 0,
+        ease: Expo.easeInOut,
+        duration: 2,
+        delay: -1,
+        stagger: .2
+    })
+
+    t1.from("#herofooter", {
+        y: -10,
+        opacity: 0,
+        duration: 1.5,
+        delay: -1,
+        ease: Expo.easeInOut
+    })
+}
+
+var timeout;
+
+function circleChaptaKaro() {
+    //define default scale value
+    var xscale = 1;
+    var yscale = 1;
+
+    var xprev = 0;
+    var yprev = 0;
+    window.addEventListener("mousemove", function (dets) {
+        clearTimeout(timeout);
+        xscale = gsap.utils.clamp(.8, 1, 2, dets.clientX - xprev);
+        yscale = gsap.utils.clamp(.8, 1, 2, dets.clientY - yprev);
+
+        xprev = dets.clientX;
+        yprev = dets.clientY;
+
+
+        ciorcleMouseFollower(xscale, yscale);
+
+        timeout = setTimeout(function () {
+            document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1, 1)`;
+        }, 100);
+
+    });
+}
+
+
+
+function ciorcleMouseFollower(xscale, yscale) {
+    window.addEventListener("mousemove", function (dets) {
+        this.document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale},${yscale})`;
+    })
+}
+
+circleChaptaKaro();
+ciorcleMouseFollower();
+firstPageAnim();
+
+//teeno element ko select karo, uske  baad teno par ek mousemove lagao, jab mouseve
+
+document.querySelectorAll(".elem")
+    .forEach(function (elem) {
+        var rotate = 0;
+        var diffrot = 0;
+        
+        elem.addEventListener("mouseleave", function (dets) {
+            gsap.to(elem.querySelector("img"), {
+                opacity: 0,
+            });
+        });
+
+        elem.addEventListener("mousemove", function (dets) {
+            var diff = dets.clientY - elem.getBoundingClientRect().top;
+            diffrot = dets.clientX - rotate;
+            rotate = dets.clientX;
+
+            gsap.to(elem.querySelector("img"), {
+                opacity: 1,
+                ease: Power1,
+                top: diff,
+                left: dets.clientX,
+                rotate: gsap.utils.clamp(-20, 20, diffrot),
+
+            });
+        });
+    });
+
+    
